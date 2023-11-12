de8678f913123497208751e22278e4cb0c3b61b4
Mohammad Ali Sk <mohammadalisk050@gmail.com>
2023-11-12 10:03:04 +0530

    Day_11

--- P_S/P_S_2/script.js
+++ P_S/P_S_2/script.js
@@ -19,26 +19,24 @@ ScrollTrigger.scrollerProxy("#main", {
   pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
 });
 }
-
-function circle(){
+function circle(x,y){
   let circle = document.querySelector(".circle");
   window.addEventListener("mousemove",(dets) => {
     // circle.style.delay = 2;
+    circle.style.display = "inline-block";
     circle.style.left = dets.clientX + "px";
     circle.style.top = dets.clientY + "px";
+    // circle.style.transform = `scale${x}px,${y}px`
+    // circle.style.transform = `scale${x}px,${y}px``;
   })
-
 };
-
 var tl = gsap.timeline();
-
 tl.from('.bounding-elem',{
   opacity:0,
   duration: .7,
   y:100,
   stagger:.2,
 });
-
 tl.from('.bounding-elem-2',{
   opacity:0,
   // duration:.9,
@@ -52,6 +50,44 @@ tl.from('.hero-footer',{
 
   delay:-1,
 })
-
 loco();
-circle();
+// circle();
+function circleChapta(){
+  var xscale = 1;
+  var yscale = 1;
+
+  var xprev = 0;
+  var yprev = 0;
+  window.addEventListener("mousemove", (dets) =>  {
+
+    xscale = gsap.utils.clamp(.8, 1.2, dets.clientX-xprev);
+    yscale = gsap.utils.clamp(.8, 1.2, dets.clientY-yprev);
+    
+    xprev = dets.clientX;
+    yprev = dets.clientY;
+    
+    circle(xscale,yscale);
+  })
+
+}
+circleChapta();
+
+
+document.querySelectorAll(".elem").forEach((elem) => {
+  elem.addEventListener("mousemove", (dets) => {
+
+    var diff = dets.clientY - elem.getBoundingClientRect().top;
+    gsap.to(elem.querySelector("#imgage"),{
+      opacity:1,
+      ease:Power1,
+      top:diff,
+      left:dets.clientX,
+
+
+    })
+  
+
+    
+  })
+})
+
